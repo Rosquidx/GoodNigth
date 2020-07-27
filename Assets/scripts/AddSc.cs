90e58369043bb98abf60236f199e5e8366dc40a6
Felipe Marin <56197583+Rosquidx@users.noreply.github.com>
2020-07-26 20:14:09 -0500

    GoodNight
    Esta es una nueva versión, tenemos puntajes y moustros que te quitan puntaje, ten cuidado con ellos.
    
    La idea es que cojas todas las calaveras que estan en el mapa pero sin tocar la cruz. las bolas que caen, porque sino pierdes, recuerda llegar hasta la cruz al final del mapa.

---
+++ Assets/scripts/AddSc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSc : MonoBehaviour
+{
+    public int Puntaje = 20;
+
+    void OnDestroy()
+    {
+        Score.Socoret += Puntaje;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public static int Socoret = 0;
+    public string ScoreString = "Score";
+    // Start is called before the first frame update
+
+
+    public Text TextScore;
+
+    public static Score ScoreGame;
+
+    void Awake()
+    {
+        ScoreGame = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TextScore != null)
+        {
+            TextScore.text = ScoreString+": "+ Socoret.ToString();
+        }
+    }
+}
